c226437233b3227c4b41704888b2a944259f81c4
Marcio Del Bianco <marcio.bianco@gmail.com>
2023-05-27 00:14:06 -0300

    add mondodb connection

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import routes from "./controller/TransactionsController.js";
+import { run } from "./schema/mongodb.js";
+
 /**
  @type {import('fastify').FastifyInstance}
  */
@@ -7,6 +9,7 @@ const fastify = Fastify({ logger: true });
 const PORT = "3000";
 
 fastify.register(routes);
+fastify.register(run);
 
 fastify.listen({ port: PORT }, () => {
   fastify.log.info(`server is listening on port ${PORT}`);
